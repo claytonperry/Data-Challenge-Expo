8902b4e35bf3235a0209325dab1e372494201684
claytonperry <claytonhperry@gmail.com>
2021-02-16 14:33:02 -0500

    update to ACS 2019 5 year for fun

--- Connect to ACS.R
+++ Connect to ACS.R
@@ -11,4 +11,5 @@ census_api_key('5dc30770cc0133357b32b987a7bdd7f51e1b4a80', install = TRUE, overw
 # ACS 2019 PUMS Data Dictionary: https://www2.census.gov/programs-surveys/acs/tech_docs/pums/data_dict/PUMS_Data_Dictionary_2019.pdf
 
 #example loading PUMS
-acs19pums <- get_pums(variables = c("AGEP","ANC1P",'COW'), state ="VT", survey = "acs1", year = 2019, recode ="TRUE", key = '5dc30770cc0133357b32b987a7bdd7f51e1b4a80')
+acs_5yr_19_pums <- get_pums(variables = c("AGEP","ANC1P",'COW'), state ="VT", survey = "acs5", year = 2019, recode ="TRUE")
+
