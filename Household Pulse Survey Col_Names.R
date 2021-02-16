272db03527e09ee06b279cb78ef1c009a199389e
claytonperry <claytonhperry@gmail.com>
2021-02-16 09:30:25 -0500

    Setting up col names and skip # for pulling direct from HPS website

---
+++ Household Pulse Survey Col_Names.R
@@ -0,0 +1,3 @@
+#Employment Table 2: Employument Status and Sector of Employment, by Select Characteristics 
+employ2_weekx_cols <- c('characteristic','total','gov','priv_company', 'nonprofit','self_emp','family_biz','not_spec','not_emp','no_report')
+#skip = 7
