53b5202a2a9067291c0d2c762eb7f4464eb116c2
claytonperry <59369535+claytonperry@users.noreply.github.com>
2021-02-26 12:51:25 -0500

    added variables from our working document

--- Connect to ACS.R
+++ Connect to ACS.R
@@ -1,15 +1,16 @@
+#Set up and pull ACS data
+
 #install.packages('tidyverse')
 #install.packages('tidycensus')
-setwd('C:/Users/admin/Folders/Data Expo Project')
 
 library(tidyverse)
 library(tidycensus)
 
 #using tidycensus: https://cran.r-project.org/web/packages/tidycensus/tidycensus.pdf
 
-census_api_key('5dc30770cc0133357b32b987a7bdd7f51e1b4a80', install = TRUE, overwrite = TRUE)
+Sys.setenv(CENSUS_API_KEY = '5dc30770cc0133357b32b987a7bdd7f51e1b4a80')
 
 # ACS 2019 PUMS Data Dictionary: https://www2.census.gov/programs-surveys/acs/tech_docs/pums/data_dict/PUMS_Data_Dictionary_2019.pdf
 
 #example loading PUMS
-acs_5yr_19_pums <- get_pums(variables = c("AGEP","ANC1P",'COW'), state ="VT", survey = "acs5", year = 2019, recode ="TRUE")
+acs_1yr_19_pums <- get_pums(variables = c("AGEP",'ESR','COW', 'RAC1P','NWAV','SCHL','FESRP'), state = 'all', survey = "acs1", year = 2019, recode ="TRUE")
