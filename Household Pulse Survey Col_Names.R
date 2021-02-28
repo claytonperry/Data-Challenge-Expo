73a2257da044172b45c5d0b4b3b7b8a870d581e7
claytonperry <59369535+claytonperry@users.noreply.github.com>
2021-02-28 11:36:09 -0500

    Delete Household Pulse Survey Col_Names.R

--- Household Pulse Survey Col_Names.R
+++
@@ -1,5 +0,0 @@
-#Employment Table 2: Employument Status and Sector of Employment, by Select Characteristics 
-employ2_weekx_cols <- c('characteristic','total','gov','priv_company', 'nonprofit','self_emp','family_biz','not_spec','not_emp','no_report')
-employ2_weekx_skip <- 7
-
-
