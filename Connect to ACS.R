de75757c98e6c58997829b46895158582ab1ee19
claytonperry <claytonhperry@gmail.com>
2021-03-12 09:55:41 -0500

    add new vars

--- Connect to ACS.R
+++ Connect to ACS.R
@@ -14,4 +14,5 @@ Sys.setenv(CENSUS_API_KEY = '5dc30770cc0133357b32b987a7bdd7f51e1b4a80')
 # ACS 2019 PUMS Data Dictionary: https://www2.census.gov/programs-surveys/acs/tech_docs/pums/data_dict/PUMS_Data_Dictionary_2019.pdf
 
 #example loading PUMS
-acs_1yr_19_pums <- get_pums(variables = c("AGEP",'ESR','COW', 'RAC1P','NWAV','SCHL','FESRP'), state = 'all', survey = "acs1", year = 2019, recode ="TRUE")
+acs_1yr_19_pums <- get_pums(variables = c('SEX',"AGEP",'ESR','COW', 'RAC1P','NWAV','SCHL','FESRP','HISP','POWSP','ST','PWGTP'),
+                            state = 'all', survey = "acs1", year = 2019, recode ="TRUE", rep_weights = 'person')
