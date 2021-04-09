8dc71c190476b78909ce240d92cd6d46aac9dd15
Clayton Perry <cperry1601@gmail.com>
2021-04-09 12:20:07 -0400

    duplicated some steps to get full set, added another round to grab unemp count. will turn it into a loop one day

--- Pull BLS Data.R
+++ Pull BLS Data.R
@@ -21,7 +21,9 @@ product <- 'LAU'
 #Set area we're interested in, in this case every state
 
 
-areas <- paste0('ST',states$st,'00000000000')
+areas <- data.frame(paste0('ST',states$st,'00000000000'),states$stusps)
+                    
+colnames(areas) <- c('code','state')
 
 #Set measurement we're interested in, in this case unemployment rate
 
@@ -31,21 +33,38 @@ measurement <- measures %>%
 
 #Put it all together as seriesIDs
 
-series <- paste0(product,areas,measurement)
+
+series1 <- paste0(product,areas$code[areas$state <= 'MO'],measurement)
+series2 <- paste0(product,areas$code[areas$state > 'MO' & areas$state < 'WY'],measurement)
+series3 <- paste0(product,areas$code[areas$state == 'WY'],measurement)
 
 #Create API payload
 
-payload <- list('seriesid' = series,
+payload1 <- list('seriesid' = series1,
         'startyear' = 2020,
         'endyear' = 2021)
 
+payload2 <- list('seriesid' = series2,
+                 'startyear' = 2020,
+                 'endyear' = 2021)
+
+payload3 <- list('seriesid' = series3,
+                 'startyear' = 2020,
+                 'endyear' = 2021)
+
 #pull API response from payload
 
-df <- blsAPI(payload,return_data_frame = TRUE)
+df1 <- blsAPI(payload1,return_data_frame = TRUE)
+
+df2 <- blsAPI(payload2,return_data_frame = TRUE)
+
+df3 <- blsAPI(payload3,return_data_frame = TRUE)
 
 #add normal statecode back
 
-df2 <- df %>%
+unemprate <- df1 %>%
+  rbind(df2) %>%
+  rbind(df3) %>%
   mutate(st = substr(seriesID,6,7),
          code = substr(seriesID,19,20),
          value = as.numeric(value),
@@ -55,6 +74,50 @@ df2 <- df %>%
   mutate(state = stusps) %>%
   select(year,month,value,measure,state)
 
-df2 %>%
-  select(state) %>%
-  summary.factor()
+## Pull unemployment total
+
+measurement <- measures %>%
+  filter(measure == 'unemployment count') %>%
+  pull(code)
+
+#Put it all together as seriesIDs
+
+
+series1 <- paste0(product,areas$code[areas$state <= 'MO'],measurement)
+series2 <- paste0(product,areas$code[areas$state > 'MO' & areas$state < 'WY'],measurement)
+series3 <- paste0(product,areas$code[areas$state == 'WY'],measurement)
+
+#Create API payload
+
+payload1 <- list('seriesid' = series1,
+                 'startyear' = 2020,
+                 'endyear' = 2021)
+
+payload2 <- list('seriesid' = series2,
+                 'startyear' = 2020,
+                 'endyear' = 2021)
+
+payload3 <- list('seriesid' = series3,
+                 'startyear' = 2020,
+                 'endyear' = 2021)
+
+#pull API response from payload
+
+df1 <- blsAPI(payload1,return_data_frame = TRUE)
+
+df2 <- blsAPI(payload2,return_data_frame = TRUE)
+
+df3 <- blsAPI(payload3,return_data_frame = TRUE)
+
+
+unemplevel <- df1 %>%
+  rbind(df2) %>%
+  rbind(df3) %>%
+  mutate(st = substr(seriesID,6,7),
+         code = substr(seriesID,19,20),
+         value = as.numeric(value),
+         month = substr(periodName,0,3)) %>%
+  left_join(measures, by = 'code') %>%
+  left_join(states, by = 'st') %>%
+  mutate(state = stusps) %>%
+  select(year,month,value,measure,state)
