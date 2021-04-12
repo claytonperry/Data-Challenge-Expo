125c36861098d8f15345eb157ec752f21c01a73b
Clayton Perry <cperry1601@gmail.com>
2021-04-12 09:16:45 -0400

    expanded 'Clay's section' at end

--- COVID related unemployment rates.R
+++ COVID related unemployment rates.R
@@ -69,20 +69,21 @@ write_sheet(total2, ss = '1M_WzK_o4eRZm1aLTX3Ey7P-jXQsxjBwO4MXD4gdaGWw', sheet =
 
 #gs4_auth()
 
-#df <- puf_df %>%
-#  group_by(EST_ST,WEEK) %>%
-#  summarise(total_pop_n = sum(PWEIGHT),
-#            total_unemp_n = sum(PWEIGHT[ANYWORK == 2]),
-#            covid_unemp_n = sum(PWEIGHT[RSNNOWRK %in% c(8,9,10,11)])) %>%
-#  rename(week = WEEK) %>%
-#  mutate(fips = str_pad(EST_ST,2,side = 'left',pad = '0'),
-#         covid_unemp_rt = covid_unemp_n/total_pop_n,
-#         total_unemp_rt = total_unemp_n/total_pop_n,
-#         covid_unemp_prop = covid_unemp_n/total_unemp_n) %>%
-#  full_join(states, by = 'fips') %>%
-#  ungroup() %>%
-#  select(state, week, covid_unemp_rt, total_unemp_rt, covid_unemp_prop) %>%
-#  gather('measurement','value',-c(1:2))
+df <- puf_df %>%
+  group_by(EST_ST,WEEK) %>%
+  summarise(total_pop_n = sum(PWEIGHT),
+            total_unemp_n = sum(PWEIGHT[ANYWORK == 2]),
+            covid_unemp_n = sum(PWEIGHT[RSNNOWRK %in% c(8,9,10,11)])) %>%
+  rename(week = WEEK) %>%
+  mutate(fips = str_pad(EST_ST,2,side = 'left',pad = '0'),
+         covid_unemp_rt = covid_unemp_n/total_pop_n,
+         total_unemp_rt = total_unemp_n/total_pop_n,
+         covid_unemp_prop = covid_unemp_n/total_unemp_n) %>%
+  full_join(states, by = 'fips') %>%
+  full_join(schedule, by = 'week') %>%
+  ungroup() %>%
+  select(state, week, midpoint, covid_unemp_rt, total_unemp_rt, covid_unemp_prop) %>%
+  gather('measurement','value',-c(1:3))
 
-#write_sheet(df, ss = '1M_WzK_o4eRZm1aLTX3Ey7P-jXQsxjBwO4MXD4gdaGWw', sheet = 'Sheet1')
+write_sheet(df, ss = '1M_WzK_o4eRZm1aLTX3Ey7P-jXQsxjBwO4MXD4gdaGWw', sheet = 'Sheet1')
 
