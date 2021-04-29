c28a3f022cbd998d493d407b1181306c554581d5
Clayton Perry <cperry1601@gmail.com>
2021-04-29 14:12:52 -0400

    add tidyverse, add pull for civ pop

--- Pull_BLS_Data.R
+++ Pull_BLS_Data.R
@@ -1,6 +1,8 @@
 #Set up and Pull BLS data
 
+#install.packages('tidyverse')
 #install.packages('blsAPI')
+library(tidyverse)
 library(blsAPI)
 
 states <- read.csv('https://gist.githubusercontent.com/dantonnoriega/bf1acd2290e15b91e6710b6fd3be0a53/raw/11d15233327c8080c9646c7e1f23052659db251d/us-state-ansi-fips.csv',
@@ -111,6 +113,54 @@ df3 <- blsAPI(payload3,return_data_frame = TRUE)
 
 
 unemplevel <- df1 %>%
+  rbind(df2) %>%
+  rbind(df3) %>%
+  mutate(st = substr(seriesID,6,7),
+         code = substr(seriesID,19,20),
+         value = as.numeric(value),
+         month = substr(periodName,0,3)) %>%
+  left_join(measures, by = 'code') %>%
+  left_join(states, by = 'st') %>%
+  mutate(state = stusps) %>%
+  select(year,month,value,measure,state)
+
+## Pull civ noninstitutional total
+
+measurement <- measures %>%
+  filter(measure == 'civilian noninstitutional population') %>%
+  pull(code)
+
+#Put it all together as seriesIDs
+
+
+series1 <- paste0(product,areas$code[areas$state <= 'MO'],measurement)
+series2 <- paste0(product,areas$code[areas$state > 'MO' & areas$state < 'WY'],measurement)
+series3 <- paste0(product,areas$code[areas$state == 'WY'],measurement)
+
+#Create API payload
+
+payload1 <- list('seriesid' = series1,
+                 'startyear' = 2020,
+                 'endyear' = 2021)
+
+payload2 <- list('seriesid' = series2,
+                 'startyear' = 2020,
+                 'endyear' = 2021)
+
+payload3 <- list('seriesid' = series3,
+                 'startyear' = 2020,
+                 'endyear' = 2021)
+
+#pull API response from payload
+
+df1 <- blsAPI(payload1,return_data_frame = TRUE)
+
+df2 <- blsAPI(payload2,return_data_frame = TRUE)
+
+df3 <- blsAPI(payload3,return_data_frame = TRUE)
+
+
+civtotal <- df1 %>%
   rbind(df2) %>%
   rbind(df3) %>%
   mutate(st = substr(seriesID,6,7),
