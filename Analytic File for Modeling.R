8604f432a8620e45eb1da822c4804a3b81c14b2b
Chrystine Tadler <chrystinetadler@Chrystines-MBP.attlocal.net>
2021-04-29 14:28:56 -0400

    fixing stuff

--- Analytic File for Modeling.R
+++ Analytic File for Modeling.R
@@ -6,7 +6,8 @@ library(tidyverse)
 
 #HH Pulse data
 #requires running 'Pull HH Pulse Data into R'
+#requires running 'Pull covid data'
 pulse <- puf_df %>%
   filter(WEEK %in% c(2,6,10,13,14,17,19,21,22,24,26)) %>%
-  mutate(Imsi = ifelse(RSNNOWRK==8|RSNNOWRK==9|RSNNOWRK==10|RSNNOWRK==11), 1, 0) %>%
-  select(WEEK,RSNNOWRK,EST_ST,EGENDER,PWEIGHT)
+  mutate(Imsi = ifelse(RSNNOWRK==8|RSNNOWRK==9|RSNNOWRK==10|RSNNOWRK==11, 1, 0)) %>%
+  select(WEEK,RSNNOWRK,EST_ST,EGENDER,PWEIGHT,Imsi)
