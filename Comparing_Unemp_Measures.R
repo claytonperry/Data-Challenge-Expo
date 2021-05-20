159824c5bb63eeb0be9af0261c5ee3a32f5360dd
claytonperry <claytonhperry@gmail.com>
2021-05-20 12:03:28 -0400

    init commit

---
+++ Comparing_Unemp_Measures.R
@@ -0,0 +1,35 @@
+#Compare BLS and HHS Pulse unemployment numbers
+
+#Step 0: Prerequisites
+# Pull_BLS_Data.R
+# Pull HH Pulse Data into R.R
+
+
+df <- puf_df %>%
+  group_by(EST_ST,WEEK) %>%
+  summarise(hhp_total_unemp_n = sum(PWEIGHT[ANYWORK == 2]),
+            hhp_covid_unemp_n = sum(PWEIGHT[RSNNOWRK %in% c(8,9,10,11)])) %>%
+  rename(week = WEEK) %>%
+  mutate(fips = str_pad(EST_ST,2,side = 'left',pad = '0')) %>%
+  full_join(states, by = 'fips') %>%
+  inner_join(schedule, by = 'week') %>%
+  inner_join(civtotal, by = c('yearmonth','state')) %>%
+  rename(bls_civpop = value) %>%
+  mutate(hhp_total_unemp_rt = hhp_total_unemp_n/bls_civpop,
+         hhp_covid_unemp_rt = hhp_covid_unemp_n/bls_civpop,
+         hhp_covid_unemp_prop = hhp_covid_unemp_n/hhp_total_unemp_n) %>%
+  inner_join(unemprate, by = c('yearmonth','state')) %>%
+  rename(bls_unemp_rt = value) %>%
+  inner_join(unemplevel, by = c('yearmonth','state')) %>%
+  rename(bls_unemp_n = value) %>%
+  mutate(bls_unemp_rt = bls_unemp_rt *0.01,
+         bls_unemp_rt_calc = bls_unemp_n/bls_civpop, 
+         bls_hhp_unemp_prop = bls_unemp_n/hhp_total_unemp_n,
+         adj_covid_unemp_n = bls_hhp_unemp_prop*hhp_covid_unemp_n,
+         adj_covid_unemp_rt = adj_covid_unemp_n/bls_civpop,
+         adj_covid_unemp_prop = adj_covid_unemp_rt/bls_unemp_rt) %>%
+  ungroup() %>%
+  select(state, yearmonth, bls_civpop, bls_unemp_n, hhp_total_unemp_n, bls_hhp_unemp_prop, bls_unemp_rt, bls_unemp_rt_calc, hhp_total_unemp_rt,
+         hhp_covid_unemp_n, hhp_covid_unemp_rt, hhp_covid_unemp_prop, adj_covid_unemp_n, adj_covid_unemp_rt, adj_covid_unemp_prop)
+
+write_sheet(df, ss = '1-bP6OPVrStqe0O_tlzFG3QYLXRfOdqg-4lYvXEraXfI', sheet = 'Compare Unemp')  
