2a1fd932aea8064b7ad157bdddec29926b453979
claytonperry <claytonhperry@gmail.com>
2021-05-23 21:01:08 -0400

    remove gather steps, change to laboforce

--- CP_Create_First_Viz.R
+++ CP_Create_First_Viz.R
@@ -16,30 +16,28 @@ df <- puf_df %>%
   mutate(fips = str_pad(EST_ST,2,side = 'left',pad = '0')) %>%
   full_join(states, by = 'fips') %>%
   inner_join(schedule, by = 'week') %>%
-  inner_join(civtotal, by = c('yearmonth','state')) %>%
-  rename(bls_civpop = value) %>%
-  mutate(hhp_total_unemp_rt = hhp_total_unemp_n/bls_civpop,
-         hhp_covid_unemp_rt = hhp_covid_unemp_n/bls_civpop,
+  inner_join(labforce, by = c('yearmonth','state')) %>%
+  rename(bls_labforce = value) %>%
+  mutate(hhp_total_unemp_rt = hhp_total_unemp_n/bls_labforce,
+         hhp_covid_unemp_rt = hhp_covid_unemp_n/bls_labforce,
          hhp_covid_unemp_prop = hhp_covid_unemp_n/hhp_total_unemp_n) %>%
   inner_join(unemprate, by = c('yearmonth','state')) %>%
   rename(bls_unemp_rt = value) %>%
   inner_join(unemplevel, by = c('yearmonth','state')) %>%
   rename(bls_unemp_n = value) %>%
   mutate(bls_unemp_rt = bls_unemp_rt *0.01,
-         bls_unemp_rt_calc = bls_unemp_n/bls_civpop, 
+         bls_unemp_rt_calc = bls_unemp_n/bls_labforce, 
          bls_hhp_unemp_prop = bls_unemp_n/hhp_total_unemp_n,
          adj_covid_unemp_n = bls_hhp_unemp_prop*hhp_covid_unemp_n,
-         adj_covid_unemp_rt = adj_covid_unemp_n/bls_civpop,
+         adj_covid_unemp_rt = adj_covid_unemp_n/bls_labforce,
          adj_covid_unemp_prop = adj_covid_unemp_n/bls_unemp_n) %>%
   ungroup() %>%
   select(state, yearmonth, monthname, 
          hhp_covid_unemp_rt, hhp_total_unemp_rt, hhp_covid_unemp_prop,
          bls_unemp_rt, bls_unemp_rt_calc,
          adj_covid_unemp_rt, adj_covid_unemp_prop) %>%
-  gather('empmeasurement','empvalue',-c(1:3)) %>%
   inner_join(confmonthly, by = c('yearmonth', 'state')) %>%
-  inner_join(deathmonthly, by = c('yearmonth', 'state')) %>%
-  gather('covidmeasure','covidvalue', -c(1:5))
+  inner_join(deathmonthly, by = c('yearmonth', 'state'))
 
 write_sheet(df, ss = '1M_WzK_o4eRZm1aLTX3Ey7P-jXQsxjBwO4MXD4gdaGWw', sheet = 'Sheet1')
 
