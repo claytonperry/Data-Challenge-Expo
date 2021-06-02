d611d1b979e3cf514ff522bb46483a87dc61e914
claytonperry <claytonhperry@gmail.com>
2021-06-02 09:52:07 -0400

    init commit

---
+++ Estimate Model 2.R
@@ -0,0 +1,5 @@
+## Estimate Model 2: Logistic regression to model whether a person is in the civilian labor force on number of confirmed COVID cases and gender.
+
+## Run First:
+## 1. Read in IPUMS CPS.R
+## 2. Pull covid data.R
