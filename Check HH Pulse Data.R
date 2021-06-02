0db8bf64825d4ff13c5c31b0d13be56d2cd6fd6c
claytonperry <claytonhperry@gmail.com>
2021-06-02 09:51:56 -0400

    add weighted sums and ratios

--- Check HH Pulse Data.R
+++ Check HH Pulse Data.R
@@ -12,10 +12,22 @@ df <- puf_df %>%
          sex = ifelse(EGENDER == 1, 'male','female'))  %>%
   full_join(states, by = 'fips') %>%
   inner_join(schedule, by = 'week') %>%
-  select(state,week,year,EGENDER,yearmonth,monthname,sex,ANYWORK,RSNNOWRK) %>%
+  select(state,week,year,EGENDER,yearmonth,monthname,sex,ANYWORK,RSNNOWRK,PWEIGHT) %>%
+  mutate(labforceif = ifelse(ANYWORK %in% c(1,2), 1, 0),
+         anyworkif = ifelse(ANYWORK == 2, 1, 0),
+         rsnnowrkif = ifelse(RSNNOWRK %in% c(8,9,10,11), 1, 0)) %>%
   group_by(state,year,monthname,yearmonth,sex) %>%
   summarise(n = n(),
+            labforce = sum(ANYWORK %in% c(1,2)),
             anywork_2 = sum(ANYWORK == 2),
-            rsnnowrk_covid = sum(RSNNOWRK %in% c(9,10,11,12)))
+            rsnnowrk_covid = sum(RSNNOWRK %in% c(8,9,10,11)),
+            weighted_n = sum(PWEIGHT),
+            weighted_lab = sum(labforceif*PWEIGHT),
+            weighted_anywork2 = sum(anyworkif * PWEIGHT),
+            weighted_rsnnowrkcov = sum(rsnnowrkif * PWEIGHT)) %>%
+  mutate(labprop = weighted_lab/weighted_n,
+         unemp_outof_lab = weighted_anywork2/weighted_lab,
+         covid_outof_lab = weighted_rsnnowrkcov/weighted_lab,
+         covid_outof_unemp = weighted_rsnnowrkcov/weighted_anywork2)
 
 write_sheet(df, ss = '1-bP6OPVrStqe0O_tlzFG3QYLXRfOdqg-4lYvXEraXfI', sheet = 'Compare HH Pulse Values')  
