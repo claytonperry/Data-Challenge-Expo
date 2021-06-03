793ceb877121048f51f50cc43ac486ac667ad5bc
claytonperry <claytonhperry@gmail.com>
2021-06-02 21:55:22 -0400

    cleaning up

--- Read in IPUMS CPS.R
+++ Read in IPUMS CPS.R
@@ -1,15 +1,25 @@
 
-
+#install.packages('expss')
 #install.packages('tidyverse')
 #install.packages('ipumsr')
 #install.packages('googledrive')
 
-
+library(expss)
 library(tidyverse)
+library(tidycensus)
 library(ipumsr)
 library(googledrive)
 
+fips <- tidycensus::fips_codes %>%
+  select(state,state_code,state_name) %>%
+  distinct() %>%
+  rename(stusps = state,
+         fips = state_code,
+         state = state_name)
+
 cps_files <- drive_find(pattern = 'cps_00001')
+
+1
 
 temp1 <- tempfile(fileext = '.csv.gz')
 dl <- drive_download(
@@ -21,4 +31,10 @@ dl <- drive_download(
   as_id(cps_files[[which(cps_files$name == 'cps_00001.xml'),2]]), path = temp2, overwrite = T
 )
 
-CPS <- read_ipums_micro(temp2, data_file = temp1)
+CPS_raw <- read_ipums_micro(temp2, data_file = temp1) 
+
+CPS <- CPS_raw %>%
+  mutate(yearmonth = paste(YEAR, month.name[MONTH],sep = " "),
+         fips = str_pad(STATEFIP,2,side = 'left',pad = '0')) %>%
+  inner_join(fips, by = 'fips')
+  
