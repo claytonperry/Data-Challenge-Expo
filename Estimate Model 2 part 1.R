d2602c3e86a0cdf520f829ea3e04ebf41f7dc572
Clayton Perry <cperry1601@gmail.com>
2021-06-28 17:52:10 -0400

    add comprehensive function

--- Estimate Model 2 part 1.R
+++ Estimate Model 2 part 1.R
@@ -27,9 +27,12 @@ fade2 <- function(x, na.rm = FALSE) (1 / (1 + exp(d - min2_ind - 8)))
 fade3 <- function(x, na.rm = FALSE) (1 / (1 + exp(d - min3_ind - 8)))
 
 z1  <- function(x, na.rm = FALSE) (fade1 * l1_ind)
-z2  <- function(x, na.rm = FALSE) (fade1 * l1_ind)
-z3  <- function(x, na.rm = FALSE) (fade1 * l1_ind)
+z2  <- function(x, na.rm = FALSE) (fade2 * l2_ind)
+z3  <- function(x, na.rm = FALSE) (fade3 * l3_ind)
 
+comprehensive1 <- function(x, na.rm = FALSE) ((1/ (1 + exp(d - min(d[x == 1]) - 8))) * ifelse(x = 1, 1, 0))
+comprehensive2 <- function(x, na.rm = FALSE) ((1/ (1 + exp(d - min(d[x == 2]) - 8))) * ifelse(x = 2, 1, 0))
+comprehensive3 <- function(x, na.rm = FALSE) ((1/ (1 + exp(d - min(d[x == 3]) - 8))) * ifelse(x = 3, 1, 0))
 
 st_pop <- acs_2019_1yr_pums %>%
   group_by(ST) %>%
@@ -56,6 +59,26 @@ analytic2_1 <- st_pop %>%
   mutate(across('C', list(f1 = fade1, f2 = fade2, f3 = fade3)),
          across('C', list(z1 = z1, z2 = z2, z3 = z3)))
 
+## COMPREHENSIVE FUNCTION VERSION
+  
+  analytic2_1 <- st_pop %>%
+    inner_join(confdaily, by = 'state') %>%
+    inner_join(restrictions, by = c('state','date')) %>%
+    group_by(state) %>%
+    mutate(d = row_number()) %>%
+    ungroup() %>%
+    mutate(sinfd = 0.5 * (sin((d/20.69) - 1.82) + 1),
+           sfd = ifelse(d <= 366,
+                        (abs(d - 183)/366) + 0.5,
+                        (abs(d - 549)/365) + 0.5),
+           x1d = ifelse(d >= 150,
+                        sfd * sinfd,
+                        (d/150) * (0.5 * (sin((150/20.69) - 1.82) + 1)) * (abs(150 - 183)/366) + 0.5),
+           cds = newcases/pop) %>%
+    group_by(state) %>%
+    mutate(across('C',list(comp1 = comprehensive1, comp2 = comprehensive2, comp3 = comprehensive3))) %>%
+    ungroup()
+  
 # Step 2: Define Survey Design
 
 design <- svydesign(ids = ~1, data = analytic2_1)
