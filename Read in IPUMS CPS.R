260413076204365a796c8cf0be7d2cefb16b9410
Chrystine Tadler <chrystinetadler@Chrystines-MBP.attlocal.net>
2021-07-23 12:32:27 -0400

    CPS: added binned education var and a QC crosstab

--- Read in IPUMS CPS.R
+++ Read in IPUMS CPS.R
@@ -45,6 +45,19 @@ CPS <- read_ipums_micro(temp2, data_file = temp1)  %>%
                             RACE == 200 ~ 3,
                             RACE %in% c(651,652) ~ 4,
                             RACE == 999 & HISPAN %in% c('901','902') ~ 9,
-                            TRUE ~ 5)) %>%
+                            TRUE ~ 5),
+         EEDUC = case_when(EDUC < 40 ~ 1,
+                           EDUC < 73 ~ 2,
+                           EDUC < 80 ~ 3,
+                           EDUC < 90 ~ 4,
+                           EDUC < 100 ~ 5,
+                           EDUC < 120 ~ 6,
+                           EDUC < 200 ~ 7,)) %>%
   inner_join(fips, by = 'fips')
+
+#QC creation of agebin and eeduc (raceth is too gross to QC)
+source("http://pcwww.liv.ac.uk/~william/R/crosstab.r")
+crosstab(CPS, row.vars = "AGE", col.vars = "agebin", type = "f")
+crosstab(CPS, row.vars = "EDUC", col.vars = "EEDUC", type = "f")
+
 
