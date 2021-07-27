9831dc325eae037b588616ef04856c3e94c69c56
claytonperry <claytonhperry@gmail.com>
2021-07-26 21:16:37 -0400

    add new vars to account for more/less levels among certain restrictions

--- Estimate Model 2 part 0.R
+++ Estimate Model 2 part 0.R
@@ -17,6 +17,7 @@ l0_ind <- function(x, na.rm = FALSE) {ifelse(x == 0, 1, 0)}
 l1_ind <- function(x, na.rm = FALSE) {ifelse(x == 1, 1, 0)}
 l2_ind <- function(x, na.rm = FALSE) {ifelse(x == 2, 1, 0)}
 l3_ind <- function(x, na.rm = FALSE) {ifelse(x == 3, 1, 0)}
+l4_ind <- function(x, na.rm = FALSE) {ifelse(x == 4, 1, 0)}
 
 min1_ind <- function(x, na.rm = FALSE) {min(d[which(x == 1)])}
 min2_ind <- function(x,d, na.rm = FALSE) (min(d[x == 2]))
@@ -52,7 +53,7 @@ analytic2_0 <- st_pop %>%
                       sfd * sinfd,
                       (d/150) * (0.5 * (sin((150/20.69) - 1.82) + 1)) * (abs(150 - 183)/366) + 0.5),
          pds = newcases_d/pop,
-         across(C1:C8,list(i0 = l0_ind, i1 = l1_ind, i2 = l2_ind, i3 = l3_ind))) %>%
+         across(C1:C8,list(i0 = l0_ind, i1 = l1_ind, i2 = l2_ind, i3 = l3_ind, i4 = l4_ind))) %>%
   group_by(state) %>%
   mutate(C1_min0 = min(d[which(C1==0)]),
           C1_min1 = min(d[which(C1==1)]),
@@ -65,15 +66,14 @@ analytic2_0 <- st_pop %>%
           C3_min0 = min(d[which(C3==0)]),
           C3_min1 = min(d[which(C3==1)]),
           C3_min2 = min(d[which(C3==2)]),
-          C3_min3 = min(d[which(C3==3)]),
           C4_min0 = min(d[which(C4==0)]),
           C4_min1 = min(d[which(C4==1)]),
           C4_min2 = min(d[which(C4==2)]),
           C4_min3 = min(d[which(C4==3)]),
+          C4_min4 = min(d[which(C4==4)]),
           C5_min0 = min(d[which(C5==0)]),
           C5_min1 = min(d[which(C5==1)]),
           C5_min2 = min(d[which(C5==2)]),
-          C5_min3 = min(d[which(C5==3)]),
           C6_min0 = min(d[which(C6==0)]),
           C6_min1 = min(d[which(C6==1)]),
           C6_min2 = min(d[which(C6==2)]),
@@ -81,11 +81,11 @@ analytic2_0 <- st_pop %>%
           C7_min0 = min(d[which(C7==0)]),
           C7_min1 = min(d[which(C7==1)]),
           C7_min2 = min(d[which(C7==2)]),
-          C7_min3 = min(d[which(C7==3)]),
           C8_min0 = min(d[which(C8==0)]),
           C8_min1 = min(d[which(C8==1)]),
           C8_min2 = min(d[which(C8==2)]),
-          C8_min3 = min(d[which(C8==3)])) %>%
+          C8_min3 = min(d[which(C8==3)]),
+          C8_min4 = min(d[which(C8==4)])) %>%
   ungroup() %>%
   mutate(across(contains('C'), function(x) replace(x, is.infinite(x), 0))) %>%
   mutate(C1_fade0 = 1 / (1 + exp(d - C1_min0 - 8)),
@@ -99,15 +99,14 @@ analytic2_0 <- st_pop %>%
          C3_fade0 = 1 / (1 + exp(d - C3_min0 - 8)),
          C3_fade1 = 1 / (1 + exp(d - C3_min1 - 8)),
          C3_fade2 = 1 / (1 + exp(d - C3_min2 - 8)),
-         C3_fade3 = 1 / (1 + exp(d - C3_min3 - 8)),
          C4_fade0 = 1 / (1 + exp(d - C4_min0 - 8)),
          C4_fade1 = 1 / (1 + exp(d - C4_min1 - 8)),
          C4_fade2 = 1 / (1 + exp(d - C4_min2 - 8)),
          C4_fade3 = 1 / (1 + exp(d - C4_min3 - 8)),
+         C4_fade4 = 1 / (1 + exp(d - C4_min4 - 8)),
          C5_fade0 = 1 / (1 + exp(d - C5_min0 - 8)),
          C5_fade1 = 1 / (1 + exp(d - C5_min1 - 8)),
          C5_fade2 = 1 / (1 + exp(d - C5_min2 - 8)),
-         C5_fade3 = 1 / (1 + exp(d - C5_min3 - 8)),
          C6_fade0 = 1 / (1 + exp(d - C6_min0 - 8)),
          C6_fade1 = 1 / (1 + exp(d - C6_min1 - 8)),
          C6_fade2 = 1 / (1 + exp(d - C6_min2 - 8)),
@@ -115,11 +114,11 @@ analytic2_0 <- st_pop %>%
          C7_fade0 = 1 / (1 + exp(d - C7_min0 - 8)),
          C7_fade1 = 1 / (1 + exp(d - C7_min1 - 8)),
          C7_fade2 = 1 / (1 + exp(d - C7_min2 - 8)),
-         C7_fade3 = 1 / (1 + exp(d - C7_min3 - 8)),
          C8_fade0 = 1 / (1 + exp(d - C8_min0 - 8)),
          C8_fade1 = 1 / (1 + exp(d - C8_min1 - 8)),
          C8_fade2 = 1 / (1 + exp(d - C8_min2 - 8)),
          C8_fade3 = 1 / (1 + exp(d - C8_min3 - 8)),
+         C8_fade4 = 1 / (1 + exp(d - C8_min4 - 8)),
          C1_z0 = C1_fade0 * C1_i0,
          C1_z1 = C1_fade1 * C1_i1,
          C1_z2 = C1_fade2 * C1_i2,
@@ -131,15 +130,14 @@ analytic2_0 <- st_pop %>%
          C3_z0 = C3_fade0 * C3_i0,
          C3_z1 = C3_fade1 * C3_i1,
          C3_z2 = C3_fade2 * C3_i2,
-         C3_z3 = C3_fade3 * C3_i3,
          C4_z0 = C4_fade0 * C4_i0,
          C4_z1 = C4_fade1 * C4_i1,
          C4_z2 = C4_fade2 * C4_i2,
          C4_z3 = C4_fade3 * C4_i3,
+         C4_z4 = C4_fade4 * C4_i4,
          C5_z0 = C5_fade0 * C5_i0,
          C5_z1 = C5_fade1 * C5_i1,
          C5_z2 = C5_fade2 * C5_i2,
-         C5_z3 = C5_fade3 * C5_i3,
          C6_z0 = C6_fade0 * C6_i0,
          C6_z1 = C6_fade1 * C6_i1,
          C6_z2 = C6_fade2 * C6_i2,
@@ -147,11 +145,11 @@ analytic2_0 <- st_pop %>%
          C7_z0 = C7_fade0 * C7_i0,
          C7_z1 = C7_fade1 * C7_i1,
          C7_z2 = C7_fade2 * C7_i2,
-         C7_z3 = C7_fade3 * C7_i3,
          C8_z0 = C8_fade0 * C8_i0,
          C8_z1 = C8_fade1 * C8_i1,
          C8_z2 = C8_fade2 * C8_i2,
-         C8_z3 = C8_fade3 * C8_i3) %>%
+         C8_z3 = C8_fade3 * C8_i3,
+         C8_z4 = C8_fade4 * C8_i4) %>%
   filter(pds > 0, complete.cases(.))
 
 # Step 3: Prepare for loop
@@ -164,10 +162,10 @@ final_list <- list()
 
 for (i in unique(analytic2_0$state)) {
   glm_list[[i]] <- glm(pds ~ x1d + C1_z1 + C1_z2 + C1_z3 +
-                         C2_z1 + C2_z2 + C2_z3 + C3_z1 + C3_z2 + C3_z3 +
-                         C4_z1 + C4_z2 + C4_z3 + C5_z1 + C5_z2 + C5_z3 +
-                         C6_z1 + C6_z2 + C6_z3 + C7_z1 + C7_z2 + C7_z3 +
-                         C8_z1 + C8_z2 + C8_z3,
+                         C2_z1 + C2_z2 + C2_z3 + C3_z1 + C3_z2 +
+                         C4_z1 + C4_z2 + C4_z3 + C4_z4 + C5_z1 + C5_z2 +
+                         C6_z1 + C6_z2 + C6_z3 + C7_z1 + C7_z2 +
+                         C8_z1 + C8_z2 + C8_z3 + C8_z4,
                        subset = state == i, data = analytic2_0,  family = Gamma)
   predictions_list[[i]] <- predict(glm_list[[i]], type = 'response')
   attributes(predictions_list[[i]]) <- NULL
@@ -179,10 +177,10 @@ aic_list <- list()
 
 for (i in unique(analytic2_0$state)) {
   glm <- glm(pds ~ x1d + C1_z1 + C1_z2 + C1_z3 +
-                  C2_z1 + C2_z2 + C2_z3 + C3_z1 + C3_z2 + C3_z3 +
-                  C4_z1 + C4_z2 + C4_z3 + C5_z1 + C5_z2 + C5_z3 +
-                  C6_z1 + C6_z2 + C6_z3 + C7_z1 + C7_z2 + C7_z3 +
-                  C8_z1 + C8_z2 + C8_z3, subset = state == i, data = analytic2_0, family = Gamma)
+               C2_z1 + C2_z2 + C2_z3 + C3_z1 + C3_z2 +
+               C4_z1 + C4_z2 + C4_z3 + C4_z4 + C5_z1 + C5_z2 +
+               C6_z1 + C6_z2 + C6_z3 + C7_z1 + C7_z2 +
+               C8_z1 + C8_z2 + C8_z3 + C8_z4, subset = state == i, data = analytic2_0, family = Gamma)
   a <- tidy(glm)
   aic <- AIC(glm)
   results_list[[i]] <- cbind(i,a)
