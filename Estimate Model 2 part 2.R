c8552b172e513c505315a5adb8ff059c762083a4
Chrystine Tadler <chrystinetadler@Chrystines-MBP.attlocal.net>
2021-08-01 12:29:39 -0400

    Model 2.2 program - commented out 2 restrictions versions, added code for richard's subset CA analysis, added a samp_size variable for the expected compare output

--- Estimate Model 2 part 2.R
+++ Estimate Model 2 part 2.R
@@ -1,3 +1,4 @@
+
 library(tidyverse)
 library(survey)
 library(broom)
@@ -29,52 +30,52 @@ final_list <- list()
 
 #run state-wise loop to produce final dataframes
 
-for (i in unique(analytic2_2$state)) {
-  glm_list[[i]] <- glm(CLF ~ newcases_hat + SEX + agebin + raceth + EEDUC +
-                         C1_c0 + C1_c1 + C1_c2 + C1_c3 +
-                         C2_c0 + C2_c1 + C2_c2 + C2_c3 +
-                         C3_c0 + C3_c1 + C3_c2 + 
-                         C4_c0 + C4_c1 + C4_c2 + C4_c3 + C4_c4 +
-                         C5_c0 + C5_c1 + C5_c2 + 
-                         C6_c0 + C6_c1 + C6_c2 + C6_c3 +
-                         C7_c0 + C7_c1 + C7_c2 + 
-                         C8_c0 + C8_c1 + C8_c2 + C8_c3 + C8_c4, subset = state == i, data = analytic2_2, family = binomial)
-  predictions_list[[i]] <- predict(glm_list[[i]], type = 'response')
-  attributes(predictions_list[[i]]) <- NULL
-  final_list[[i]] <- cbind(analytic2_2 %>% filter(state == i),data.frame(predict = predictions_list[[i]]))
-}
+#for (i in unique(analytic2_2$state)) {
+#  glm_list[[i]] <- glm(CLF ~ newcases_hat + SEX + agebin + raceth + EEDUC +
+#                         C1_c0 + C1_c1 + C1_c2 + C1_c3 +
+#                         C2_c0 + C2_c1 + C2_c2 + C2_c3 +
+#                         C3_c0 + C3_c1 + C3_c2 + 
+#                         C4_c0 + C4_c1 + C4_c2 + C4_c3 + C4_c4 +
+#                         C5_c0 + C5_c1 + C5_c2 + 
+#                         C6_c0 + C6_c1 + C6_c2 + C6_c3 +
+#                         C7_c0 + C7_c1 + C7_c2 + 
+#                         C8_c0 + C8_c1 + C8_c2 + C8_c3 + C8_c4, subset = state == i, data = analytic2_2, family = binomial)
+#  predictions_list[[i]] <- predict(glm_list[[i]], type = 'response')
+#  attributes(predictions_list[[i]]) <- NULL
+#  final_list[[i]] <- cbind(analytic2_2 %>% filter(state == i),data.frame(predict = predictions_list[[i]]))
+#}
 
-m2_2_expected_v_df_v1 <- do.call('rbind',final_list)
+#m2_2_expected_v_df_v1 <- do.call('rbind',final_list)
 
-results_list <- list()
+#results_list <- list()
 
-for (i in unique(analytic2_2$state)) {
-  glm <- glm(CLF ~ newcases_hat + SEX + agebin + raceth + EEDUC +
-               C1_c0 + C1_c1 + C1_c2 + C1_c3 +
-               C2_c0 + C2_c1 + C2_c2 + C2_c3 +
-               C3_c0 + C3_c1 + C3_c2 + 
-               C4_c0 + C4_c1 + C4_c2 + C4_c3 + C4_c4 +
-               C5_c0 + C5_c1 + C5_c2 + 
-               C6_c0 + C6_c1 + C6_c2 + C6_c3 +
-               C7_c0 + C7_c1 + C7_c2 + 
-               C8_c0 + C8_c1 + C8_c2 + C8_c3 + C8_c4, subset = state == i, data = analytic2_2, family = binomial)
-  a <- tidy(glm)
-  aic <- AIC(glm)
-  results_list[[i]] <- cbind(i,a)
-  aic_list[[i]] <- cbind(i,aic)
-}
+#for (i in unique(analytic2_2$state)) {
+#  glm <- glm(CLF ~ newcases_hat + SEX + agebin + raceth + EEDUC +
+#               C1_c0 + C1_c1 + C1_c2 + C1_c3 +
+#               C2_c0 + C2_c1 + C2_c2 + C2_c3 +
+#               C3_c0 + C3_c1 + C3_c2 + 
+#               C4_c0 + C4_c1 + C4_c2 + C4_c3 + C4_c4 +
+#               C5_c0 + C5_c1 + C5_c2 + 
+#               C6_c0 + C6_c1 + C6_c2 + C6_c3 +
+#               C7_c0 + C7_c1 + C7_c2 + 
+#               C8_c0 + C8_c1 + C8_c2 + C8_c3 + C8_c4, subset = state == i, data = analytic2_2, family = binomial)
+#  a <- tidy(glm)
+#  aic <- AIC(glm)
+#  results_list[[i]] <- cbind(i,a)
+#  aic_list[[i]] <- cbind(i,aic)
+#}
 
 
-m2_2_results_v1 <- do.call('rbind',results_list)
-m2_2_aic_v1 <- data.frame(do.call('rbind',aic_list))
-m2_2_expecteds_v1_compare <- m2_2_expected_v_df_v1 %>%
-  group_by(state,yearmonth) %>%
-  summarise(CLF = sum(CLF),
-            predictions = sum(predict))
+#m2_2_results_v1 <- do.call('rbind',results_list)
+#m2_2_aic_v1 <- data.frame(do.call('rbind',aic_list))
+#m2_2_expecteds_v1_compare <- m2_2_expected_v_df_v1 %>%
+#  group_by(state,yearmonth) %>%
+#  summarise(CLF = sum(CLF),
+#            predictions = sum(predict))
 
-range_write(m2_2_results_v1, ss = '1w89IU3xGa__wGtFBG-sHBmo7QSGs1NXF9rItS0m2fdo', range = 'Model 2.2 v1!A:F')
-range_write(m2_2_aic_v1, ss = '1w89IU3xGa__wGtFBG-sHBmo7QSGs1NXF9rItS0m2fdo', range = 'Model 2.2 v1!H:I')
-range_write(m2_2_expecteds_v1_compare, ss = '1w89IU3xGa__wGtFBG-sHBmo7QSGs1NXF9rItS0m2fdo', range = 'Model 2.2 v1!K:N')
+#range_write(m2_2_results_v1, ss = '1w89IU3xGa__wGtFBG-sHBmo7QSGs1NXF9rItS0m2fdo', range = 'Model 2.2 v1!A:F')
+#range_write(m2_2_aic_v1, ss = '1w89IU3xGa__wGtFBG-sHBmo7QSGs1NXF9rItS0m2fdo', range = 'Model 2.2 v1!H:I')
+#range_write(m2_2_expecteds_v1_compare, ss = '1w89IU3xGa__wGtFBG-sHBmo7QSGs1NXF9rItS0m2fdo', range = 'Model 2.2 v1!K:N')
 
 #version using sum1
 
@@ -106,7 +107,8 @@ m2_2_results_v2 <- do.call('rbind',results_list)
 m2_2_aic_v2 <- data.frame(do.call('rbind',aic_list))
 m2_2_expecteds_v2_compare <- m2_2_expected_v_df_v1 %>%
   group_by(state,yearmonth) %>%
-  summarise(CLF = sum(CLF),
+  summarise(samp_size = n(),
+            CLF = sum(CLF),
             predictions = sum(predict))
 
 range_write(m2_2_results_v2, ss = '1w89IU3xGa__wGtFBG-sHBmo7QSGs1NXF9rItS0m2fdo', range = 'Model 2.2 v2!A:F')
@@ -115,23 +117,63 @@ range_write(m2_2_expecteds_v2_compare, ss = '1w89IU3xGa__wGtFBG-sHBmo7QSGs1NXF9r
 
 #version using sum2
 
-for (i in unique(analytic2_2$state)) {
+#for (i in unique(analytic2_2$state)) {
+#  glm_list[[i]] <- glm(CLF ~ newcases_hat + SEX + agebin + raceth + EEDUC +
+#                         C1_sum2 + C2_sum2 + C3_sum2 + C4_sum2 +
+#                         C5_sum2 + C6_sum2 + C7_sum2 + C8_sum2, subset = state == i, data = analytic2_2, family = binomial)
+#  predictions_list[[i]] <- predict(glm_list[[i]], type = 'response')
+#  attributes(predictions_list[[i]]) <- NULL
+#  final_list[[i]] <- cbind(analytic2_2 %>% filter(state == i),data.frame(predict = predictions_list[[i]]))
+#}
+
+#m2_2_expected_v_df_v3 <- do.call('rbind',final_list)
+
+#results_list <- list()
+
+#for (i in unique(analytic2_2$state)) {
+#  glm <- glm(CLF ~ newcases_hat + SEX + agebin + raceth + EEDUC +
+#               C1_sum2 + C2_sum2 + C3_sum2 + C4_sum2 +
+#               C5_sum2 + C6_sum2 + C7_sum2 + C8_sum2, subset = state == i, data = analytic2_2, family = binomial)
+#  a <- tidy(glm)
+#  aic <- AIC(glm)
+#  results_list[[i]] <- cbind(i,a)
+#  aic_list[[i]] <- cbind(i,aic)
+#}
+
+
+#m2_2_results_v3 <- do.call('rbind',results_list)
+#m2_2_aic_v3 <- data.frame(do.call('rbind',aic_list))
+#m2_2_expecteds_v3_compare <- m2_2_expected_v_df_v1 %>%
+#  group_by(state,yearmonth) %>%
+#  summarise(CLF = sum(CLF),
+#            predictions = sum(predict))
+
+#range_write(m2_2_results_v3, ss = '1w89IU3xGa__wGtFBG-sHBmo7QSGs1NXF9rItS0m2fdo', range = 'Model 2.2 v3!A:F')
+#range_write(m2_2_aic_v3, ss = '1w89IU3xGa__wGtFBG-sHBmo7QSGs1NXF9rItS0m2fdo', range = 'Model 2.2 v3!H:I')
+#range_write(m2_2_expecteds_v3_compare, ss = '1w89IU3xGa__wGtFBG-sHBmo7QSGs1NXF9rItS0m2fdo', range = 'Model 2.2 v3!K:N')
+
+
+#CHRYS - subset analysis for Richard 
+#version using sum1
+
+analytic2_2_ca <- analytic2_2 %>%
+  filter(state == 'California')
+#glm
+for (i in unique(analytic2_2_ca$state)) {
   glm_list[[i]] <- glm(CLF ~ newcases_hat + SEX + agebin + raceth + EEDUC +
-                         C1_sum2 + C2_sum2 + C3_sum2 + C4_sum2 +
-                         C5_sum2 + C6_sum2 + C7_sum2 + C8_sum2, subset = state == i, data = analytic2_2, family = binomial)
+                         C1_sum1 + C2_sum1 + C3_sum1 + C4_sum1, subset = state == i, data = analytic2_2, family = binomial)
   predictions_list[[i]] <- predict(glm_list[[i]], type = 'response')
   attributes(predictions_list[[i]]) <- NULL
   final_list[[i]] <- cbind(analytic2_2 %>% filter(state == i),data.frame(predict = predictions_list[[i]]))
 }
 
-m2_2_expected_v_df_v3 <- do.call('rbind',final_list)
+m2_2_expected_v_df_v2_ca <- do.call('rbind',final_list)
 
 results_list <- list()
 
-for (i in unique(analytic2_2$state)) {
+for (i in unique(analytic2_2_ca$state)) {
   glm <- glm(CLF ~ newcases_hat + SEX + agebin + raceth + EEDUC +
-               C1_sum2 + C2_sum2 + C3_sum2 + C4_sum2 +
-               C5_sum2 + C6_sum2 + C7_sum2 + C8_sum2, subset = state == i, data = analytic2_2, family = binomial)
+               C1_sum1 + C2_sum1 + C3_sum1 + C4_sum1, subset = state == i, data = analytic2_2, family = binomial)
   a <- tidy(glm)
   aic <- AIC(glm)
   results_list[[i]] <- cbind(i,a)
@@ -139,14 +181,15 @@ for (i in unique(analytic2_2$state)) {
 }
 
 
-m2_2_results_v3 <- do.call('rbind',results_list)
-m2_2_aic_v3 <- data.frame(do.call('rbind',aic_list))
-m2_2_expecteds_v3_compare <- m2_2_expected_v_df_v1 %>%
+m2_2_results_v2_ca <- do.call('rbind',results_list)
+m2_2_aic_v2_ca <- data.frame(do.call('rbind',aic_list))
+m2_2_expecteds_v2_ca_compare <- m2_2_expected_v_df_v2_ca %>%
   group_by(state,yearmonth) %>%
-  summarise(CLF = sum(CLF),
+  summarise(samp_size = n(),
+            CLF = sum(CLF),
             predictions = sum(predict))
 
-range_write(m2_2_results_v3, ss = '1w89IU3xGa__wGtFBG-sHBmo7QSGs1NXF9rItS0m2fdo', range = 'Model 2.2 v3!A:F')
-range_write(m2_2_aic_v3, ss = '1w89IU3xGa__wGtFBG-sHBmo7QSGs1NXF9rItS0m2fdo', range = 'Model 2.2 v3!H:I')
-range_write(m2_2_expecteds_v3_compare, ss = '1w89IU3xGa__wGtFBG-sHBmo7QSGs1NXF9rItS0m2fdo', range = 'Model 2.2 v3!K:N')
+range_write(m2_2_results_v2_ca, ss = '1w89IU3xGa__wGtFBG-sHBmo7QSGs1NXF9rItS0m2fdo', range = 'Model 2.2 v2 CAglm!A:F')
+range_write(m2_2_aic_v2_ca, ss = '1w89IU3xGa__wGtFBG-sHBmo7QSGs1NXF9rItS0m2fdo', range = 'Model 2.2 v2 CAglm!H:I')
+range_write(m2_2_expecteds_v2_ca_compare, ss = '1w89IU3xGa__wGtFBG-sHBmo7QSGs1NXF9rItS0m2fdo', range = 'Model 2.2 v2 CAglm!K:O')
 
