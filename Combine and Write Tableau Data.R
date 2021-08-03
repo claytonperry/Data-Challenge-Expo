58b79d4fee455e9aea2d438f6659e35e323a9da4
claytonperry <claytonhperry@gmail.com>
2021-08-02 20:45:38 -0400

    final step

--- Combine and Write Tableau Data.R
+++ Combine and Write Tableau Data.R
@@ -2,6 +2,10 @@
 library(googlesheets4)
 library(tidyverse)
 
+gs4_auth()
+
+1
+
 viz_data <- rbind(scenario1_viz, scenario2_viz, scenario3_viz)
 
-write_sheet(viz_data, ss = , sheet = 'Sheet1')
+write_sheet(viz_data, ss = '1U_qzjiqKhBiPqc7CMXay9NMITL949iK6MEPt8jv7hcI', sheet = 'Sheet1')
